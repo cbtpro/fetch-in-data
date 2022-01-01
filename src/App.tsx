67ca153aa5f446de1febfa9f734c222107e08e1f
陈碧滔 <cbtpro@163.com>
2022-01-01 09:57:41 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./views/home";
+import FetchInFromJSON from "./views/fetch-in-from-json";
+
+import "./App.css";
 
 function App() {
+  const home = <Home />
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/fetch-in-json">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Routes>
+        <Route path="/" element={home} />
+        <Route path="/fetch-in-json" element={<FetchInFromJSON />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/views/fetch-in-from-json.tsx
@@ -0,0 +1,125 @@
+import defaultData from "../assets/data/default.json";
+
+interface IItem {
+  id: string;
+  app_id: string;
+  community_id: string;
+  org_content: string;
+  content: {
+    text: string;
+  };
+  tags_content: string;
+  file_url: string;
+  file_name: string;
+  file_size: number;
+  file_json: {
+    name: string;
+    url: string;
+    size: number | string;
+    fileType: string;
+  }[];
+  feeds_type: number;
+  user_id: string;
+  title: string;
+  is_chosen: number;
+  is_notice: number;
+  user_type: number;
+  send_type: number;
+  zan_num: number;
+  comment_count: number;
+  created_at: string;
+  universal_union_id: string;
+  is_exercise: number;
+  is_public: number;
+  me_praise: boolean;
+  nick_name: string;
+  wx_avatar: string;
+  is_quit: number;
+  user_state: number;
+  is_guest: number;
+  work_corp_id: string;
+  work_user_id: string;
+  show_time: string;
+  praise_list: {
+    app_id: string;
+    user_id: string;
+    nick_name: string;
+    wx_avatar: string;
+    user_type: number;
+    is_guest: number;
+    community_id: string;
+    feed_id: string;
+    work_corp_id: string;
+    work_user_id: string;
+  }[];
+  commentList: {
+    list: {
+      id: number;
+      app_id: string;
+      community_id: string;
+      feeds_id: string;
+      user_id: string;
+      comment: string;
+      comment_resource: {
+        image: any[];
+        audio: any[];
+      }[];
+      type: number;
+      main_comment_id?: string;
+      reply_user_id: string;
+      reply_comment_id?: string;
+      send_type: number;
+      created_at: string;
+      praise_cnt: number;
+      nick_name: string;
+      wx_avatar: string;
+      user_type: number;
+      is_guest: number;
+      reply_nick_name: string;
+      reply_wx_avatar: string;
+      reply_user_type: number;
+      reply_is_guest: number;
+      zan_num: number;
+      show_time: string;
+      work_corp_id: string;
+      work_user_id: string;
+      reply_work_corp_id: string;
+      reply_work_user_id: string;
+    }[];
+    total_count: number;
+  };
+  is_self: boolean;
+  login_user_id: string;
+  tags: any[];
+}
+interface IRawData {
+  code: number;
+  msg: string;
+  data: IItem[];
+  feeds_num: number;
+}
+
+interface IViewData {
+  app_id: string;
+  community_id: string;
+  nick_name: string;
+  content: string;
+  created_at: string;
+}
+function FetchInFromJSON() {
+  const { feeds_num, data } = defaultData as unknown as IRawData;
+  const json: IViewData[] = data.map((item) => {
+    const { app_id, community_id, nick_name, content, created_at } = item;
+    const { text } = content;
+    return {
+      app_id,
+      community_id,
+      nick_name,
+      content: text,
+      created_at,
+    };
+  });
+  return <h1>{JSON.stringify(json, null, 2)}</h1>;
+}
+
+export default FetchInFromJSON;
