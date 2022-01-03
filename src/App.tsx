a8136bf19f5065da972924d58625aec86e3743f0
chenbitao <peter.chen@useful-person.com>
2022-01-03 16:01:39 +0800

    增加echarts支持

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./views/home";
 import FetchInFromJSON from "./views/fetch-in-from-json";
+import Report from "./views/report";
 
 import "./App.css";
 
 function App() {
-  const home = <Home />
   return (
     <HashRouter>
       <div>
@@ -18,12 +18,16 @@ function App() {
             <li>
               <Link to="/fetch-in-json">从JSON中获取数据</Link>
             </li>
+            {/* <li>
+              <Link to="/report">报表</Link>
+            </li> */}
           </ul>
         </nav>
       </div>
       <Routes>
-        <Route path="/" element={home} />
-        <Route path="/fetch-in-json" element={<FetchInFromJSON />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/fetch-in-json" element={<FetchInFromJSON />} />
+        <Route path="/report" element={<Report />}></Route>
       </Routes>
     </HashRouter>
   );
---
+++ src/views/report.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useRef, } from 'react'
+import * as echarts from 'echarts/core';
+import {
+  BarChart,
+  // 系列类型的定义后缀都为 SeriesOption
+  BarSeriesOption,
+  LineChart,
+  LineSeriesOption
+} from 'echarts/charts';
+import {
+  TitleComponent,
+  // 组件类型的定义后缀都为 ComponentOption
+  TitleComponentOption,
+  TooltipComponent,
+  TooltipComponentOption,
+  GridComponent,
+  GridComponentOption,
+  // 数据集组件
+  DatasetComponent,
+  DatasetComponentOption,
+  // 内置数据转换器组件 (filter, sort)
+  TransformComponent
+} from 'echarts/components';
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+import { CanvasRenderer } from 'echarts/renderers';
+
+// 通过 ComposeOption 来组合出一个只有必须组件和图表的 Option 类型
+type ECOption = echarts.ComposeOption<
+  | BarSeriesOption
+  | LineSeriesOption
+  | TitleComponentOption
+  | TooltipComponentOption
+  | GridComponentOption
+  | DatasetComponentOption
+>;
+
+// 注册必须的组件
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  BarChart,
+  LabelLayout,
+  UniversalTransition,
+  CanvasRenderer
+]);
+
+function Report() {
+  const reportRef = useRef<HTMLDivElement>(null)
+  let myChart: echarts.ECharts
+  const initEChart = () => {
+    myChart = echarts.init(reportRef.current as HTMLDivElement, undefined, {
+      width: 1920,
+      height: 600,
+    });
+    myChart.setOption({
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [120, 200, 150, 80, 70, 110, 130],
+          type: 'bar'
+        }
+      ]
+    });
+  }
+  const disposeECharts = () => {
+    if (myChart) {
+      myChart.dispose()
+    }
+  }
+  useEffect(() => {
+    initEChart()
+    return () => {
+      disposeECharts()
+      console.log('这里应该销毁报表组件')
+    }
+  })
+  // 接下来的使用就跟之前一样，初始化图表，设置配置项
+  return <><div ref={reportRef}>123</div></>
+}
+
+export default Report
